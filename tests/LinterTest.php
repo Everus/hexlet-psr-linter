d828aa4e01fd83edf34ff5d71ab63d2274da54ac
Mark Antipov <oper29a@gmail.com>
2016-08-07 23:28:35 +0500

    Tests for linter.

--- tests/LinterTest.php
+++ tests/LinterTest.php
@@ -6,4 +6,34 @@ use PHPUnit\Framework\TestCase;
 
 class LinterTest extends TestCase
 {
+    /**
+    * @dataProvider additionProvider
+    */
+    public function testLinter($expected, $code)
+    {
+        $linter = new Linter();
+        $report = $linter->lint($code);
+        $messages = array_map(function ($message) {
+            return $message->getMessage();
+        }, $report->getMessages());
+        $this->assertEquals($expected, $messages);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            [
+                [],
+                '<?php function test() {}'
+            ],
+            [
+                ['Method names SHOULD NOT contain a single underscore'],
+                '<?php function _underscore() {}'
+            ],
+            [
+                ['Parse Error: Syntax error, unexpected EOF, expecting \'(\' on line 1'],
+                '<?php function _underscore'
+            ]
+        ];
+    }
 }
