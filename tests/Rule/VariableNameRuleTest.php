31b08f285388cebb94d5c7420aae213e49ca3196
Mark Antipov <oper29a@gmail.com>
2016-08-07 12:59:18 +0500

    Tests for step 4.

---
+++ tests/Rule/VariableNameRuleTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace HexletPSRLinter\Rule;
+
+use PHPUnit\Framework\TestCase;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\PropertyProperty;
+use HexletPSRLinter\Linter;
+
+class VariableNameRuleTest extends TestCase
+{
+    protected function getLinterMock()
+    {
+        return $this->getMockBuilder(Linter::class)
+            ->setMethods(['addReport'])
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testStrongCase()
+    {
+        $node = new Variable('StrongTestName');
+        $linter = $this->getLinterMock();
+        $linter->expects($this->once())
+            ->method('addReport')
+            ->willReturn($linter);
+        $rule = new VariableNameRule($linter);
+        $rule->enterNode($node);
+    }
+
+    public function testCamelCase()
+    {
+        $node = new Variable('camelCase');
+        $linter = $this->getLinterMock();
+        $linter->expects($this->never())
+            ->method('addReport')
+            ->willReturn($linter);
+        $rule = new VariableNameRule($linter);
+        $rule->enterNode($node);
+    }
+
+    public function testUnderScopeCase()
+    {
+        $node = new Variable('underscope_case');
+        $linter = $this->getLinterMock();
+        $linter->expects($this->never())
+            ->method('addReport')
+            ->willReturn($linter);
+        $rule = new VariableNameRule($linter);
+        $rule->enterNode($node);
+    }
+
+    public function testUnderPrefixScopeCase()
+    {
+        $node = new Variable('_underscope_case');
+        $linter = $this->getLinterMock();
+        $linter->expects($this->once())
+            ->method('addReport')
+            ->willReturn($linter);
+        $rule = new VariableNameRule($linter);
+        $rule->enterNode($node);
+    }
+
+    public function testMixedCase()
+    {
+        $node = new Variable('Underscope_case');
+        $linter = $this->getLinterMock();
+        $linter->expects($this->once())
+            ->method('addReport')
+            ->willReturn($linter);
+        $rule = new VariableNameRule($linter);
+        $rule->enterNode($node);
+    }
+
+    public function testMixedStyleCase()
+    {
+        $node = new Variable('underscope_case');
+        $node2 = new Variable('camelCase');
+        $linter = $this->getLinterMock();
+        $linter->expects($this->once())
+            ->method('addReport')
+            ->willReturn($linter);
+        $rule = new VariableNameRule($linter);
+        $rule->enterNode($node);
+        $rule->enterNode($node2);
+    }
+
+    public function testPropertyPropertyMethod()
+    {
+        $node = new PropertyProperty('_IncorrectPropertyName');
+        $linter = $this->getLinterMock();
+        $linter->expects($this->once())
+            ->method('addReport')
+            ->willReturn($linter);
+        $rule = new VariableNameRule($linter);
+        $rule->enterNode($node);
+    }
+}
